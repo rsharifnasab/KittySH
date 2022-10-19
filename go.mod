77d891eee8cc1051d16ad0884a9fe14b2524ecd3
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2022-10-19 17:21:29 +0330

    feat: MVP

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/rsharifnasab/KittySH
 
 go 1.19
+
+require (
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
+	github.com/spf13/cobra v1.6.0
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path/filepath"
 	"syscall"
+
+	"github.com/kballard/go-shellquote"
+	"github.com/spf13/cobra"
 )
 
 const (
-	prompt = ">>> "
+	prompt = "[Yourname@Your PC]$ "
 )
 
-func readStdin(out chan string, terminate chan bool) {
-	//str, err := exec.Command("ls", "-gah").CombinedOutput()
-	//if err != nil {
-	//	println(err.Error())
-	//}
-	//fmt.Println(string(str))
-
+func setupStdin() {
 	//no buffering
 	err := exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
 	if err != nil {
@@ -31,16 +32,22 @@ func readStdin(out chan string, terminate chan bool) {
 		log.Fatalln("fatal error: cannot disable auto echo")
 	}
 
+}
+
+func readStdin(out chan string) {
+	//str, err := exec.Command("ls", "-gah").CombinedOutput()
+	//if err != nil {
+	//	println(err.Error())
+	//}
+	//fmt.Println(string(str))
+
+	setupStdin()
+
 	b := make([]byte, 1)
 	for {
-		select {
-		case <-terminate:
-			return
-		default:
-			os.Stdin.Read(b)
-			//println(b[0])
-			out <- string(b)
-		}
+		os.Stdin.Read(b)
+		//println(b[0])
+		out <- string(b)
 	}
 }
 
@@ -81,7 +88,7 @@ charLoop:
 			break charLoop
 		case "\x7F":
 			command = backspace(command)
-			println("backspace")
+			//println("backspace")
 		default:
 			fmt.Print(char)
 			command = command + string(char)
@@ -91,10 +98,80 @@ charLoop:
 	return command
 }
 
+func handleReader(reader *bufio.Reader) {
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		fmt.Print(str)
+	}
+}
+
+func executeCommand(command string) {
+	resetStdin()
+
+	commandUnix := filepath.ToSlash(command)
+	commandWords, err := shellquote.Split(commandUnix)
+	if err != nil {
+		fmt.Println("invalid qouting")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // cleanup resources eventually
+
+	// Create the command with our context
+	execCmd := exec.CommandContext(ctx, commandWords[0], commandWords[1:]...)
+	execCmd.Env = os.Environ() // TODO: check
+	execCmd.Stdin = os.Stdin
+
+	// initialize stdout and stderr before start
+	stdoutPipe, err := execCmd.StdoutPipe()
+	if err != nil {
+		fmt.Printf("unable to initialize process stdout")
+	}
+	defer stdoutPipe.Close()
+	stdoutReader := bufio.NewReader(stdoutPipe)
+
+	stderrPipe, err := execCmd.StderrPipe()
+	if err != nil {
+		fmt.Printf("unable to initialize process stdout")
+	}
+	defer stderrPipe.Close()
+	stderrReader := bufio.NewReader(stderrPipe)
+
+	// finally start the process!
+	err = execCmd.Start()
+	if err != nil {
+		switch err.(type) {
+		//   linux        , windows
+		case *fs.PathError, *exec.Error:
+			//fmt.Println(err.Error())
+			fmt.Println("invalid executable")
+		default:
+			cobra.CheckErr(err)
+		}
+	}
+
+	go handleReader(stdoutReader)
+	go handleReader(stderrReader)
+
+	pid := execCmd.Process.Pid
+	//println("process started with pid : " + pid)
+	_ = pid
+
+	// finished flag become true
+	// and check for any error
+	executeErr := execCmd.Wait()
+
+	if executeErr != nil {
+		fmt.Println(executeErr.Error()) // TODO: handle in a better way
+	}
+}
+
 func main() {
 	stdin := make(chan string, 1)
-	terminate := make(chan bool, 1)
-	go readStdin(stdin, terminate)
+	go readStdin(stdin)
 	defer resetStdin()
 
 	disableCtrlC()
@@ -105,23 +182,19 @@ commandLoop:
 		command := readCommand(stdin)
 
 		switch command {
+
+		case "":
+			break
 		case "exit":
 			break commandLoop
-
 		case "clear":
-			println("\n\n\n\n\n\n\n\n\n")
+			fmt.Print("\x1b\x5b\x48\x1b\x5b\x32\x4a")
 		default:
-
+			fmt.Println("\nrunning : " + command)
+			executeCommand(command)
 		}
 
-		//if char == "q" {
-		//	terminate <- true
-		//	break inpLoop
-		//}
-
 		fmt.Println("")
-		fmt.Println("running : " + command)
-		_ = command
 	}
 	close(stdin)
 
