dd23dda4c500e362244e5e6d88dc31ed3272a50e
Roozbeh Sharifnasab <roozbeh.sharifnasab@snapp.cab>
2023-11-06 12:48:22 +0330

    feat: use libraries to handle things better

--- go.mod
+++ go.mod
@@ -2,12 +2,11 @@ module github.com/rsharifnasab/KittySH
 
 go 1.19
 
-require (
-	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
-	github.com/spf13/cobra v1.6.0
-)
+require github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 
 require (
-	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/creack/pty v1.1.20 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/term v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,201 +1,154 @@
 package main
 
 import (
-	"bufio"
-	"context"
+	"errors"
 	"fmt"
-	"io/fs"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
 	"path/filepath"
+	"strings"
 	"syscall"
 
+	"github.com/chzyer/readline"
+	"github.com/creack/pty"
 	"github.com/kballard/go-shellquote"
-	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 const (
 	prompt = "[Yourname@Your PC]$ "
 )
 
-func setupStdin() {
-	//no buffering
-	err := exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	if err != nil {
-		log.Fatalln("fatal error: cannot disable shell buffering")
-	}
-	//no visible output
-	err = exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-	if err != nil {
-		log.Fatalln("fatal error: cannot disable auto echo")
-	}
-
-}
-
-func readStdin(out chan string) {
-	//str, err := exec.Command("ls", "-gah").CombinedOutput()
-	//if err != nil {
-	//	println(err.Error())
-	//}
-	//fmt.Println(string(str))
-
-	setupStdin()
-
-	b := make([]byte, 1)
-	for {
-		os.Stdin.Read(b)
-		//println(b[0])
-		out <- string(b)
-	}
-}
-
-func resetStdin() {
-	exec.Command("stty", "-F", "/dev/tty", "echo").Run()
-}
-
-func disableCtrlC() {
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		for {
-			<-c
-			println("Ctrl c pressed")
-			os.Exit(1)
-		}
-	}()
-}
-
-func backspace(s string) string {
-	size := len(s)
-	if size == 0 {
-		return s
-	} else {
-		return s[:size-1]
-	}
-}
-
-func readCommand(stdin chan string) string {
-	command := ""
-charLoop:
-	for {
-		// blockin
-		char := <-stdin
-
-		switch char {
-		case "\n":
-			break charLoop
-		case "\x7F":
-			command = backspace(command)
-			//println("backspace")
-		default:
-			fmt.Print(char)
-			command = command + string(char)
-		}
-
-	}
-	return command
-}
-
-func handleReader(reader *bufio.Reader) {
-	for {
-		str, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
-		fmt.Print(str)
-	}
-}
-
-func executeCommand(command string) {
-	resetStdin()
-
+func execute(command string) {
 	commandUnix := filepath.ToSlash(command)
+
 	commandWords, err := shellquote.Split(commandUnix)
 	if err != nil {
 		fmt.Println("invalid qouting")
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel() // cleanup resources eventually
+	execCmd := exec.Command(commandWords[0], commandWords[1:]...)
+	execCmd.Env = os.Environ()
 
-	// Create the command with our context
-	execCmd := exec.CommandContext(ctx, commandWords[0], commandWords[1:]...)
-	execCmd.Env = os.Environ() // TODO: check
-	execCmd.Stdin = os.Stdin
-
-	// initialize stdout and stderr before start
-	stdoutPipe, err := execCmd.StdoutPipe()
+	ptmx, err := pty.Start(execCmd)
 	if err != nil {
-		fmt.Printf("unable to initialize process stdout")
+		log.Printf("cannot execute command, %s", err)
+		return
 	}
-	defer stdoutPipe.Close()
-	stdoutReader := bufio.NewReader(stdoutPipe)
 
-	stderrPipe, err := execCmd.StderrPipe()
-	if err != nil {
-		fmt.Printf("unable to initialize process stdout")
-	}
-	defer stderrPipe.Close()
-	stderrReader := bufio.NewReader(stderrPipe)
+	defer func() { _ = ptmx.Close() }() // Best effort.
 
-	// finally start the process!
-	err = execCmd.Start()
-	if err != nil {
-		switch err.(type) {
-		//   linux        , windows
-		case *fs.PathError, *exec.Error:
-			//fmt.Println(err.Error())
-			fmt.Println("invalid executable")
-		default:
-			cobra.CheckErr(err)
+	// Handle pty size.
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGWINCH)
+	go func() {
+		for range ch {
+			if err := pty.InheritSize(os.Stdin, ptmx); err != nil {
+				log.Printf("error resizing pty: %s", err)
+			}
 		}
+	}()
+	ch <- syscall.SIGWINCH                        // Initial resize.
+	defer func() { signal.Stop(ch); close(ch) }() // Cleanup signals when done.
+
+	// Set stdin in raw mode.
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
 	}
+	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }() // Best effort.
 
-	go handleReader(stdoutReader)
-	go handleReader(stderrReader)
+	// Copy stdin to the pty and the pty to stdout.
+	// NOTE: The goroutine will keep reading until the next keystroke before returning.
+	go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
+	_, _ = io.Copy(os.Stdout, ptmx)
+}
 
-	pid := execCmd.Process.Pid
-	//println("process started with pid : " + pid)
-	_ = pid
-
-	// finished flag become true
-	// and check for any error
-	executeErr := execCmd.Wait()
-
-	if executeErr != nil {
-		fmt.Println(executeErr.Error()) // TODO: handle in a better way
+// Function constructor - constructs new function for listing given directory
+func listFiles(path string) func(string) []string {
+	return func(line string) []string {
+		names := make([]string, 0)
+		files, _ := os.ReadDir(path)
+		for _, f := range files {
+			names = append(names, f.Name())
+		}
+		return names
 	}
 }
 
+func buildCompleter() *readline.PrefixCompleter {
+	return readline.NewPrefixCompleter(
+		readline.PcItem("exit"),
+		readline.PcItem("clear"),
+		readline.PcItem("setprompt"),
+		readline.PcItem("cd", readline.PcItemDynamic(listFiles("./"))),
+		readline.PcItem("vim", readline.PcItemDynamic(listFiles("./"))),
+		readline.PcItem("nvim", readline.PcItemDynamic(listFiles("./"))),
+	)
+}
+
+func filterInput(r rune) (rune, bool) {
+	switch r {
+	// block CtrlZ feature
+	case readline.CharCtrlZ:
+		return r, false
+	}
+	return r, true
+}
+
 func main() {
-	stdin := make(chan string, 1)
-	go readStdin(stdin)
-	defer resetStdin()
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          prompt,
+		HistoryFile:     "/tmp/kitty-hist.tmp",
+		AutoComplete:    buildCompleter(),
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
 
-	disableCtrlC()
+		HistorySearchFold:   true,
+		FuncFilterInputRune: filterInput,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	l.CaptureExitSignal()
 
-commandLoop:
+	log.SetOutput(l.Stderr())
+
 	for {
-		print(prompt)
-		command := readCommand(stdin)
+		line, err := l.Readline()
+		if errors.Is(err, readline.ErrInterrupt) {
+			if len(line) == 0 {
+				break
+			}
 
-		switch command {
-
-		case "":
+			continue
+		} else if errors.Is(err, io.EOF) {
 			break
-		case "exit":
-			break commandLoop
-		case "clear":
-			fmt.Print("\x1b\x5b\x48\x1b\x5b\x32\x4a")
-		default:
-			fmt.Println("\nrunning : " + command)
-			executeCommand(command)
 		}
 
-		fmt.Println("")
-	}
-	close(stdin)
+		line = strings.TrimSpace(line)
 
+		switch {
+		case strings.HasPrefix(line, "setprompt"):
+			if len(line) <= len("setprompt") {
+				log.Println("setprompt <prompt>")
+
+				break
+			}
+
+			l.SetPrompt(line[len("setprompt"):] + " ")
+		case line == "exit":
+			goto exit
+		case line == "clear":
+			os.Stdout.Write([]byte("\x1b\x5b\x48\x1b\x5b\x32\x4a"))
+		case line == "":
+		default:
+			execute(line)
+		}
+	}
+exit:
 }
